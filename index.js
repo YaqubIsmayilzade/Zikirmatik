cd19e869e85d06a6eb1f07cd48b3d7f790f318a9
YaqubIsmayilzade <73059722+YaqubIsmayilzade@users.noreply.github.com>
2025-01-25 12:44:32 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ let number = document.getElementById("number");
 document.getElementById("add").onclick= function(){
     count++;
     number.textContent = count;
+    window.navigator.vibrate(1000);
 }
 
 document.getElementById("reset").onclick = function(){
     count = 0;
-    number.textContent=count
+    number.textContent=count;
+    window.navigator.vibrate(1000);
 }
