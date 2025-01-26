950fbedad1e9d7be240b745b8f0437e708ef4ee7
YaqubIsmayilzade <73059722+YaqubIsmayilzade@users.noreply.github.com>
2025-01-26 04:13:17 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ let number = document.getElementById("number");
 document.getElementById("add").onclick= function(){
     count++;
     number.textContent = count;
-    window.navigator.vibrate(1000);
+    window.navigator.vibrate(80);
 }
 
 document.getElementById("reset").onclick = function(){
     count = 0;
     number.textContent=count;
-    window.navigator.vibrate(1000);
+    window.navigator.vibrate(200);
 }
